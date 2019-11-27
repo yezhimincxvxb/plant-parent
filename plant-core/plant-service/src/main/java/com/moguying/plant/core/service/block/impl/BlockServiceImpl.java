8e76a4e17152573a033328ed7b9af09e3c27efe0
yezhimin <395995486@qq.com>
2019-11-27 09:50:47 +0800

    1.获取大棚id 2.邀请注册列表倒叙，可分页

--- plant-core/plant-service/src/main/java/com/moguying/plant/core/service/block/impl/BlockServiceImpl.java
+++ plant-core/plant-service/src/main/java/com/moguying/plant/core/service/block/impl/BlockServiceImpl.java
@@ -19,13 +19,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 @Service
 @Slf4j
-public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements BlockService {
-    
+public class BlockServiceImpl extends ServiceImpl<BlockDAO, Block> implements BlockService {
+
     @Autowired
     private BlockDAO blockDAO;
 
@@ -34,29 +35,29 @@ public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements Blo
     @DS("read")
     public PageResult<Block> blockList(Integer page, Integer size, Block where) {
         IPage<Block> pageResult = blockDAO.selectSelective(new Page<>(page, size), where);
-        return new PageResult<>(pageResult.getTotal(),pageResult.getRecords());
+        return new PageResult<>(pageResult.getTotal(), pageResult.getRecords());
     }
 
     @Override
     @DS("write")
     public ResultData<Integer> addBlock(Block block) {
-        ResultData<Integer> resultData = new ResultData<>(MessageEnum.ERROR,-1);
+        ResultData<Integer> resultData = new ResultData<>(MessageEnum.ERROR, -1);
         Block useBlock = blockDAO.selectBlockByNumber(block.getNumber());
-        if(useBlock != null)
+        if (useBlock != null)
             return resultData.setMessageEnum(MessageEnum.BLOCK_NUMBER_USED);
         block.setAddTime(new Date());
 
         BigDecimal[] result = block.getTotalAmount().divideAndRemainder(block.getPerPrice());
-        if(result[1].compareTo(new BigDecimal("0")) != 0 )
+        if (result[1].compareTo(new BigDecimal("0")) != 0)
             return resultData.setMessageEnum(MessageEnum.PARAMETER_ERROR);
 
-        if(block.getMinPlant() > block.getMaxPlant() || block.getMinPlant() <= 0 || block.getMaxPlant() > result[0].intValue())
+        if (block.getMinPlant() > block.getMaxPlant() || block.getMinPlant() <= 0 || block.getMaxPlant() > result[0].intValue())
             return resultData.setMessageEnum(MessageEnum.PARAMETER_ERROR);
 
         block.setTotalCount(result[0].intValue());
         block.setLeftCount(result[0].intValue());
 
-        if(blockDAO.insert(block) > 0) {
+        if (blockDAO.insert(block) > 0) {
             return resultData.setMessageEnum(MessageEnum.SUCCESS).setData(block.getId());
         }
         return resultData;
@@ -66,24 +67,24 @@ public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements Blo
     @DS("write")
     public ResultData<Integer> updateBlock(Integer id, Block update) {
 
-        ResultData<Integer> resultData = new ResultData<>(MessageEnum.ERROR,0);
+        ResultData<Integer> resultData = new ResultData<>(MessageEnum.ERROR, 0);
         //如果棚区已种植过，则不允许修改种植分类及生长周期
         int count = blockDAO.blockIsFree(id);
-        if(count > 0){
+        if (count > 0) {
             update.setSeedType(null);
             update.setGrowDays(null);
         }
 
         Block block = blockDAO.selectById(id);
-        if(block == null)
+        if (block == null)
             return resultData.setMessageEnum(MessageEnum.BLOCK_NOT_EXISTS);
 
         Block useBlock = blockDAO.selectBlockByNumber(update.getNumber());
-        if(useBlock != null && !useBlock.getId().equals(id))
+        if (useBlock != null && !useBlock.getId().equals(id))
             return resultData.setMessageEnum(MessageEnum.BLOCK_NUMBER_USED);
 
         update.setId(block.getId());
-        if(blockDAO.updateById(update) > 0)
+        if (blockDAO.updateById(update) > 0)
             return resultData.setMessageEnum(MessageEnum.SUCCESS).setData(1);
 
         return resultData;
@@ -93,16 +94,16 @@ public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements Blo
     @Override
     @DS("write")
     public ResultData<Integer> deleteBlock(Integer id) {
-        ResultData<Integer> resultData = new ResultData<>(MessageEnum.ERROR,0);
+        ResultData<Integer> resultData = new ResultData<>(MessageEnum.ERROR, 0);
         Block block = blockDAO.selectById(id);
-        if(block == null)
+        if (block == null)
             return resultData.setMessageEnum(MessageEnum.BLOCK_NOT_EXISTS);
 
         //存在已种植订单且未采摘的不可删除
-        if(blockDAO.blockIsFree(id) > 0)
+        if (blockDAO.blockIsFree(id) > 0)
             return resultData.setMessageEnum(MessageEnum.BLOCK_CANNOT_DELETE);
 
-        if(blockDAO.deleteById(id) > 0){
+        if (blockDAO.deleteById(id) > 0) {
             return resultData.setMessageEnum(MessageEnum.SUCCESS).setData(1);
         }
         return resultData;
@@ -111,9 +112,9 @@ public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements Blo
     @Override
     @DS("read")
     public ResultData<Block> blockInfo(Integer id) {
-        ResultData<Block> resultData = new ResultData<>(MessageEnum.ERROR,null);
+        ResultData<Block> resultData = new ResultData<>(MessageEnum.ERROR, null);
         Block block = blockDAO.selectById(id);
-        if(block == null)
+        if (block == null)
             return resultData.setMessageEnum(MessageEnum.BLOCK_NOT_EXISTS);
         return resultData.setMessageEnum(MessageEnum.SUCCESS).setData(block);
     }
@@ -122,12 +123,12 @@ public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements Blo
     @DS("write")
     public Boolean seeBlock(Integer id) {
         Block block = blockDAO.selectById(id);
-        if(block == null)
+        if (block == null)
             return false;
         Block update = new Block();
         update.setId(id);
         update.setIsShow(!block.getIsShow());
-        if(blockDAO.updateById(update) > 0){
+        if (blockDAO.updateById(update) > 0) {
             return update.getIsShow();
         }
         return false;
@@ -138,7 +139,7 @@ public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements Blo
     @Override
     public List<BlockDetail> blockRecommend() {
         List<BlockDetail> blockDetails = blockDAO.blockRecommend();
-        blockDetails.forEach((x)->{
+        blockDetails.forEach((x) -> {
             BigDecimal interest = InterestUtil.INSTANCE.calInterest(x.getSeedPrice(), x.getInterestRates(), x.getSeedGrowDays());
             x.setSaleAmount(x.getSeedPrice().add(interest));
             double percent = new Double(x.getHasCount()) / x.getTotalCount();
@@ -155,15 +156,34 @@ public class BlockServiceImpl extends ServiceImpl<BlockDAO,Block> implements Blo
         where.setIsShow(true);
         where.setState(BlockStateEnum.OPEN.getState());
         List<Block> blocks = blockDAO.selectSelective(where);
-        if(null == blocks || blocks.isEmpty())
+        if (null == blocks || blocks.isEmpty())
             return where;
-        if(blocks.size() == 1)
+        if (blocks.size() == 1)
             return blocks.get(0);
         where.setLeftCount(0);
-        for(Block block : blocks){
-            if(block.getLeftCount() > where.getLeftCount())
+        for (Block block : blocks) {
+            if (block.getLeftCount() > where.getLeftCount())
                 where = block;
         }
         return where;
+    }
+
+    @Override
+    @DS("read")
+    public Block getOne() {
+        List<Integer> days = Arrays.asList(
+                BlockStateEnum.GROW_DAYS_30.getState(),
+                BlockStateEnum.GROW_DAYS_45.getState(),
+                BlockStateEnum.GROW_DAYS_60.getState());
+        QueryWrapper<Block> queryWrapper = new QueryWrapper<Block>()
+                .eq("is_show", true)
+                .eq("state", true)
+                .eq("is_delete", false)
+                .gt("left_count", 0)
+                .in("grow_days", days)
+                .orderByAsc("grow_days")
+                .orderByAsc("left_count")
+                .last("limit 1");
+        return blockDAO.selectOne(queryWrapper);
     }
 }
