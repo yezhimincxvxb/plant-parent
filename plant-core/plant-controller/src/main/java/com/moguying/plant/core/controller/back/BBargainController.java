5b0f3f8f5e99552897badd226dca09e491f38570
yezhimin <395995486@qq.com>
2019-11-28 18:15:05 +0800

    1.砍价列表下载

--- plant-core/plant-controller/src/main/java/com/moguying/plant/core/controller/back/BBargainController.java
+++ plant-core/plant-controller/src/main/java/com/moguying/plant/core/controller/back/BBargainController.java
@@ -1,13 +1,22 @@
 package com.moguying.plant.core.controller.back;
 
+import com.moguying.plant.constant.MessageEnum;
+import com.moguying.plant.core.entity.DownloadInfo;
 import com.moguying.plant.core.entity.PageResult;
 import com.moguying.plant.core.entity.PageSearch;
+import com.moguying.plant.core.entity.ResponseData;
+import com.moguying.plant.core.entity.admin.AdminUser;
 import com.moguying.plant.core.entity.bargain.vo.BackBargainDetailVo;
 import com.moguying.plant.core.entity.bargain.vo.BargainVo;
+import com.moguying.plant.core.entity.system.vo.SessionAdminUser;
 import com.moguying.plant.core.service.bargain.BargainDetailService;
+import com.moguying.plant.core.service.common.DownloadService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/bargain")
@@ -17,11 +26,27 @@ public class BBargainController {
     @Autowired
     private BargainDetailService bargainDetailService;
 
+    @Value("${excel.download.dir}")
+    private String downloadDir;
+
     /**
      * 后台砍价详情列表
      */
     @PostMapping("/list")
     public PageResult<BackBargainDetailVo> bargainList(@RequestBody PageSearch<BargainVo> search) {
         return bargainDetailService.bargainList(search.getPage(), search.getSize(), search.getWhere());
+    }
+
+    /**
+     * 后台砍价详情列表下载
+     */
+    @PostMapping("/excel")
+    public ResponseData<Integer> bargainListExcel(@SessionAttribute(SessionAdminUser.sessionKey) AdminUser user,
+                                                  @RequestBody PageSearch<BargainVo> search,
+                                                  HttpServletRequest request) {
+        DownloadInfo downloadInfo = new DownloadInfo("砍价列表", request.getServletContext(), user.getId(), downloadDir);
+        PageResult<BackBargainDetailVo> pageResult = bargainDetailService.bargainList(search.getPage(), search.getSize(), search.getWhere());
+        new Thread(new DownloadService<>(pageResult.getData(), BackBargainDetailVo.class, downloadInfo)).start();
+        return new ResponseData<>(MessageEnum.SUCCESS.getMessage(), MessageEnum.SUCCESS.getState());
     }
 }
